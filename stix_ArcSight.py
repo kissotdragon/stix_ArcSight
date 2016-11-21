2c343cfbc1fe4a27fb4677cc9dd1b7321506596a
richardmhope <richmhope@gmail.com>
2016-11-21 11:33:58 +1100

    Update stix_ArcSight.py

--- stix_ArcSight.py
+++ stix_ArcSight.py
@@ -90,34 +90,34 @@ def syslog(message,
 
 
 def extractObservable(args, obs, values):
-    typ = obs["properties"]["xsi:type"]
+    typ = obs['properties']['xsi:type']
 
     if args[0].type and args[0].type != typ:
         return
 
     val = None
-    if typ == "AddressObjectType":
+    if typ == 'AddressObjectType':
 
         # Handle if Address_Value is a plain string or one with datatype
-        if isinstance(obs["properties"]["address_value"], basestring):
-            val = obs["properties"]["address_value"]
-        elif 'value' in obs["properties"]["address_value"]:
-            val = obs["properties"]["address_value"]["value"]
-    elif typ == "URIObjectType" or typ == "DomainNameObjectType" or typ == "HostnameObjectType":
-        val = obs["properties"]["value"]
+        if isinstance(obs['properties']['address_value'], basestring):
+            val = obs['properties']['address_value']
+        elif 'value' in obs['properties']['address_value']:
+            val = obs['properties']['address_value']['value']
+    elif typ == 'URIObjectType' or typ == 'DomainNameObjectType' or typ == 'HostnameObjectType':
+        val = obs['properties']['value']
         if 'value' in val:
-            val = obs["properties"]["value"]["value"]
+            val = obs['properties']['value']['value']
         else:
-            val = obs["properties"]["value"]
-    elif typ == "UserAccountObjectType":
-        val = obs["properties"]["username"]
-    elif typ == "FileObjectType":
+            val = obs['properties']['value']
+    elif typ == 'UserAccountObjectType':
+        val = obs['properties']['username']
+    elif typ == 'FileObjectType':
         val = []
-        theList = obs["properties"]["hashes"][0]
+        theList = obs['properties']['hashes'][0]
         if len(theList['simple_hash_value']) > 2:
             val.append(theList['simple_hash_value'])
         else:
-            val.append(obs["properties"]["hashes"][0]['simple_hash_value'][
+            val.append(obs['properties']['hashes'][0]['simple_hash_value'][
                 'value'])
 
     if val:
@@ -129,12 +129,12 @@ def extractObservable(args, obs, values):
             values.append(val)
     else:
         if args[0].strict:
-            raise Exception("Encountered unsupported CybOX observable type: " +
+            raise Exception('Encountered unsupported CybOX observable type: ' +
                             typ)
         else:
             print(
-                "Encountered unsupported CybOX observable type: " + typ +
-                ", ignoring...",
+                'Encountered unsupported CybOX observable type: ' + typ +
+                ', ignoring...',
                 file=sys.stderr)
 
 
@@ -145,20 +145,20 @@ def extractObservables(args, indicators):
 
         #Check if we were passed a list of indicators, or observables
         obs = indicator
-        if "observable" in indicator:
-            obs = indicator["observable"]
+        if 'observable' in indicator:
+            obs = indicator['observable']
 
         try:
             if 'object' in obs:
-                extractObservable(args, obs["object"], values)
+                extractObservable(args, obs['object'], values)
             elif 'observable_composition' in obs:
-                for observable in obs["observable_composition"]["observables"]:
+                for observable in obs['observable_composition']['observables']:
                     if 'object' in observable:
-                        extractObservable(args, observable["object"], values)
+                        extractObservable(args, observable['object'], values)
             else:
-                raise Exception("Unknown Object Type!! Please Investigate")
+                raise Exception('Unknown Object Type!! Please Investigate')
         except:
-            print("Could not handle observable/indicator:\n", file=sys.stderr)
+            print('Could not handle observable/indicator:\n', file=sys.stderr)
             pprint.pprint(indicator, sys.stderr)
             raise
 
@@ -198,13 +198,13 @@ def get_parser():
         action='store')
     parser.add_option(
         '--strict',
-        action="store_true",
-        help="Raise an error on an unsupported indicator. Defaults to simply printing to stderr."
+        action='store_true',
+        help='Raise an error on an unsupported indicator. Defaults to simply printing to stderr.'
     )
     parser.add_option(
         '--verbose',
-        action="store_true",
-        help="Print various inputs and outputs to STDERR")
+        action='store_true',
+        help='Print various inputs and outputs to STDERR')
     parser.add_option(
         '-a',
         '--arcsight',
@@ -218,67 +218,67 @@ def get_parser():
     parser.add_option(
         '-p',
         '--taxiiport',
-        default="80",
+        default='80',
         help='Port for the TAXII Server',
         action='store')
     parser.add_option(
         '-c',
-        "--collection",
-        default="default",
-        help="TAXII Data Collection to poll. Defaults to 'default'.")
+        '--collection',
+        default='default',
+        help='TAXII Data Collection to poll. Defaults to 'default'.')
     parser.add_option(
         '--taxii_endpoint',
         help='TAXII Service Endpoint. Required if -x is provided.',
         action='store')
     parser.add_option(
-        "--taxii_ssl",
+        '--taxii_ssl',
         default=None,
-        help="Set this to use SSL for the TAXII request")
+        help='Set this to use SSL for the TAXII request')
     parser.add_option(
-        "--taxii_username",
+        '--taxii_username',
         default=None,
-        help="Set this to the username for TAXII BASIC authentication, if any")
+        help='Set this to the username for TAXII BASIC authentication, if any')
     parser.add_option(
-        "--taxii_password",
+        '--taxii_password',
         default=None,
-        help="Set this to use password for TAXII BASIC authentication, if any")
+        help='Set this to use password for TAXII BASIC authentication, if any')
     parser.add_option(
-        "--taxii_cert",
+        '--taxii_cert',
         default=None,
-        help="Set this to the cert file for TAXII CERT authentication, if any")
+        help='Set this to the cert file for TAXII CERT authentication, if any')
     parser.add_option(
-        "--taxii_key",
+        '--taxii_key',
         default=None,
-        help="Set this to use password for TAXII CERT authentication, if any")
+        help='Set this to use password for TAXII CERT authentication, if any')
     parser.add_option(
-        "--proxy",
+        '--proxy',
         default=None,
-        help="Set this to use a proxy server (format http[s]://proxy:port) to access TAXII, if necessary"
+        help='Set this to use a proxy server (format http[s]://proxy:port) to access TAXII, if necessary'
     )
 
     parser.add_option(
-        "--taxii_start_time",
-        dest="begin_ts",
+        '--taxii_start_time',
+        dest='begin_ts',
         default=None,
-        help="The start timestamp (YYYY-MM-dd HH:MM:SS) in UTC " +
-        "for the taxii poll request. Defaults to None.")
+        help='The start timestamp (YYYY-MM-dd HH:MM:SS) in UTC ' +
+        'for the taxii poll request. Defaults to None.')
 
     parser.add_option(
-        "--taxii_end_time",
-        dest="end_ts",
+        '--taxii_end_time',
+        dest='end_ts',
         default=None,
-        help="The end timestamp (YYYY-MM-dd HH:MM:SS) in UTC " +
-        "for the taxii poll request. Defaults to None.")
+        help='The end timestamp (YYYY-MM-dd HH:MM:SS) in UTC ' +
+        'for the taxii poll request. Defaults to None.')
 
     return parser
 
 
 def print_help(parser):
     print(
-        "\nA utility that imports STIX documents from either a TAXII server collection or a file.\n",
+        '\nA utility that imports STIX documents from either a TAXII server collection or a file.\n',
         file=sys.stderr)
     print(
-        "All indicators and observables in the STIX document(s) will be imported into the specified reference set.\n",
+        'All indicators and observables in the STIX document(s) will be imported into the specified reference set.\n',
         file=sys.stderr)
     print(parser.format_help().strip(), file=sys.stderr)
 
@@ -288,51 +288,51 @@ def print_help(parser):
 
 def process_package_dict(args, stix_dict):
     values = []
-    key = ""
-    value = ""
+    key = ''
+    value = ''
     dest = CONFIG['DESTINATION_IP']['ip']
     dest_port = int(CONFIG['DESTINATION_IP']['port'])
-    if "observables" in stix_dict:
+    if 'observables' in stix_dict:
         values.extend(
-            extractObservables(args, stix_dict["observables"]["observables"]))
-    if "indicators" in stix_dict:
-        values.extend(extractObservables(args, stix_dict["indicators"]))
+            extractObservables(args, stix_dict['observables']['observables']))
+    if 'indicators' in stix_dict:
+        values.extend(extractObservables(args, stix_dict['indicators']))
     if len(values) > 0:
         for item in values:
             try:
                 ## send data to stdout if needed and/or save to a simple text file.
-                if re.match("^(http|https)", item):
+                if re.match('^(http|https)', item):
                     u = urlparse(item)
                     if args[0].verbose:
                         print('Web Site: %s | Path: %s' % (u.netloc, u.path))
                     if args[0].outfile:
                         with open('WebSites.txt', 'a') as mysites:
-                            mysites.writelines("%s\n" % item)
+                            mysites.writelines('%s\n' % item)
                     if args[0].arcsight:
                         cef = 'CEF:0|IHC-OSINT|Soltra|1.0|100|Known Malicious ' + 'Website' + '|1|request=' + item + ' shost=' + u.netloc + ' msg=IHC-OSINT Malicious Domain ' + u.netloc
                         time.sleep(.02)
                         syslog(cef, host=dest, port=dest_port)
-                elif re.match("[^@]+@[^@]+\.[^@]+", item):
+                elif re.match('[^@]+@[^@]+\.[^@]+', item):
                     if args[0].verbose:
                         print('Email Address: %s' % (item))
                     if args[0].outfile:
                         with open('EmailAddresses.txt', 'a') as myemails:
-                            myemails.writelines("%s\n" % item)
+                            myemails.writelines('%s\n' % item)
                     if args[0].arcsight:
                         cef = 'CEF:0|IHC-OSINT|Soltra|1.0|100|Known Malicious ' + 'Email' + '|1|suser=' + item + ' msg=IHC-OSINT Malicious Email ' + item
                         time.sleep(.02)
                         syslog(cef, host=dest, port=dest_port)
-                elif re.match("^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", item):
+                elif re.match('^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$', item):
                     if args[0].verbose:
                         print('IP Address: %s' % (item))
                     if args[0].outfile:
                         with open('MyIPs.txt', 'a') as myips:
-                            myips.writelines("%s\n" % item)
+                            myips.writelines('%s\n' % item)
                     if args[0].arcsight:
                         cef = 'CEF:0|IHC-OSINT|Soltra|1.0|100|Known Malicious ' + 'Host' + '|1|src=' + item + ' msg=IHC-OSINT Malicious IP ' + item
                         time.sleep(0.2)
                         syslog(cef, host=dest, port=dest_port)
-                elif re.match("^:", item):
+                elif re.match('^:', item):
                     if args[0].verbose:
                         item = item[2:]
                         myitem = 'http://' + item
@@ -341,22 +341,22 @@ def process_package_dict(args, stix_dict):
                         print('Domain: %s' % (d.netloc))
                     if args[0].outfile:
                         with open('AdditionalDomains.txt', 'a') as adom:
-                            adom.writelines("%s\n" % item)
+                            adom.writelines('%s\n' % item)
                     if args[0].arcsight:
                         cef = 'CEF:0|IHC-OSINT|Soltra|1.0|100|Malicious ' + 'Domain' + '|1|request=' + item + ' msg=IHC-OSINT Malicious Domain ' + d.netloc
                         time.sleep(0.2)
                         syslog(cef, host=dest, port=dest_port)
                     elif re.match(
-                            "^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$",
+                            '^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d{1,5}$',
                             item):
-                        data = item.split(":")
+                        data = item.split(':')
                         #print data
                         if args[0].verbose:
                             print('IP Address: %s | Dest Port: %s' %
                                   (data[0], data[1]))
                         if args[0].outfile:
                             with open('IPandPort.txt', 'a') as IPdom:
-                                IPdom.writelines("%s\n" % item)
+                                IPdom.writelines('%s\n' % item)
                         if args[0].arcsight:
                             cef = 'CEF:0|IHC-OSINT|Soltra|1.0|100|Known Malicious ' + 'IP and Port' + '|1|src=' + data[
                                 0] + ' dpt=' + data[
@@ -369,13 +369,13 @@ def process_package_dict(args, stix_dict):
                         print('Indicator: %s' % (item))
                     if args[0].outfile:
                         with open('Indicators.txt', 'a') as Idom:
-                            Idom.writelines("%s\n" % item)
+                            Idom.writelines('%s\n' % item)
                     if args[0].arcsight:
                         cef = 'CEF:0|IHC-OSINT|Soltra|1.0|100|Known Malicious ' + 'Indicator' + '|1|cs1=' + item + ' msg=IHC-OSINT Malicious Indicator ' + item
                         time.sleep(0.2)
                         syslog(cef, host=dest, port=dest_port)
             except ValueError:
-                print("Could not parse values..", file=sys.stderr)
+                print('Could not parse values..', file=sys.stderr)
                 print(item, file=sys.stderr)
                 raise
 
@@ -417,7 +417,7 @@ def main():
                 end_ts = None
 
         except ValueError:
-            print("Could not parse either start or end time", file=sys.stderr)
+            print('Could not parse either start or end time', file=sys.stderr)
             raise
 
         poll_req = tm11.PollRequest(
@@ -438,7 +438,7 @@ def main():
             client.set_auth_type(1)
             if not args[0].taxii_password:
                 args[0].taxii_password = getpass.getpass(
-                    "Enter your taxii password: ")
+                    'Enter your taxii password: ')
             client.set_auth_credentials({
                 'username': args[0].taxii_username,
                 'password': args[0].taxii_password
@@ -454,39 +454,39 @@ def main():
             client.set_proxy(arg[0].proxy)
 
         resp = client.call_taxii_service2(
-            args[0].taxii, args[0].taxii_endpoint + "/poll/",
+            args[0].taxii, args[0].taxii_endpoint + '/poll/',
             t.VID_TAXII_XML_11, poll_req_xml, args[0].taxiiport)
 
         response_message = t.get_message_from_http_response(resp, '0')
         response_dict = response_message.to_dict()
         indicators = 0
         if 'content_blocks' in response_dict:
-            for content in response_dict["content_blocks"]:
-                bindingId = content["content_binding"]["binding_id"]
+            for content in response_dict['content_blocks']:
+                bindingId = content['content_binding']['binding_id']
 
                 if bindingId and bindingId.startswith(
-                        "urn:stix.mitre.org:xml"):
+                        'urn:stix.mitre.org:xml'):
                     #if args[0].verbose:
-                    #    print >> sys.stderr, "RECIEVED STIX DATA:\n"
-                    #print >> sys.stderr, content["content"]
+                    #    print >> sys.stderr, 'RECIEVED STIX DATA:\n'
+                    #print >> sys.stderr, content['content']
 
                     try:
                         # Parse the information 
                         stix_pkg = STIXPackage.from_xml(
-                            io.BytesIO(content["content"]))
+                            io.BytesIO(content['content']))
                         stix_package = stix_pkg
                         indicators += process_package_dict(
                             args, stix_package.to_dict())
 
                     except ValueError:
                         print(
-                            "Could not parse STIX document: ", file=sys.stderr)
-                        print(content["content"], file=sys.stderr)
+                            'Could not parse STIX document: ', file=sys.stderr)
+                        print(content['content'], file=sys.stderr)
                         raise
 
-            print("Imported", indicators, "indicators into set")
+            print('Imported', indicators, 'indicators into set')
         else:
-            print("Invalid reponse from TAXII server", file=sys.stderr)
+            print('Invalid reponse from TAXII server', file=sys.stderr)
             pprint.pprint(response_dict, sys.stderr)
             exit(255)
 
@@ -497,13 +497,13 @@ def main():
 
         indicators = process_package_dict(args, stix_package.to_dict())
 
-        print("Imported", indicators, "indicators into set")
+        print('Imported', indicators, 'indicators into set')
 
     else:
         print(
-            "Invalid arguments. Type 'python stix_Arcsight.py --help' for usage.\n",
+            'Invalid arguments. Type 'python stix_Arcsight.py --help' for usage.\n',
             file=sys.stderr)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
